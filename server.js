5ecbafeec39fc9b77bea5f98f3340d9f5fc4d239
Marta Waplak <mwaplak@gmail.com>
2018-05-03 13:22:18 +0200

    Add project

---
+++ server.js
@@ -0,0 +1,18 @@
+var express = require('express')
+var app = express()
+app.use(express.static('logo'))
+
+app.set('view engine', 'pug')
+app.set('views', './views')
+
+app.get('/', function (req, res) {
+  res.render('login')
+})
+
+app.get('/auth/google', function (req, res) {
+  res.render('logged', {
+    username: req.query.username
+  })
+})
+
+app.listen(3000)
